a1be3709d60f367fbeeb6424a4df9a775d2dbfa7
Stallos11 <vincentdelev@yahoo.fr>
2021-04-20 11:19:28 +0200

    feat(front): item component

--- clicker/src/components/Item.vue
+++ clicker/src/components/Item.vue
@@ -1,40 +1,76 @@
 <template>
   <div id="container">
-    <p>This component has as properties : </p>
+    <!-- <p>This component has as properties : </p>
     <ul>
       <li>name : {{ name }}</li>
       <li>earning : {{ earning }}</li>
       <li>buildsCount : {{ buildsCount }}</li>
       <li>basePrice : {{ basePrice }}</li>
       <li>priceMultiplicator : {{ priceMultiplicator }}</li>
-      <li>actualPrice : {{ actualPrice }}</li>
       <li>obtentionLevel : {{ obtentionLevel }}</li>
+      <li>actualPrice : {{ actualPrice }}</li>
       <li>EPS : {{ EPS }}</li>
-    </ul>
+    </ul> -->
+    <div class="p-3 airforce dark-3 mb-3 mx-3 rounded-2 light-shadow-2 d-flex">
+      <div>
+        <div>
+          {{ name }}
+          <em class="txt-right font-s1 pr-1 pt-1">x{{ buildsCount }}</em>
+        </div>
+        <em class="font-s1 mt-2 d-block">
+          Gains/sec : {{ EPS }}
+        </em>
+      </div>
+      <div class="ml-auto d-flex fx-col">
+        <button @click="buy" class="btn small white rounded-1 shadow-1 my-auto">buy</button>
+        <em class="font-s1 mt-2 d-block">
+          Price : {{ actualPrice }}
+        </em>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 
 export default {
-  name: 'ExploreContainer',
+  name: 'Item',
   props: {
-    name: String,
-    earning: Number,
-    buildsCount: Number,
-    basePrice: Number,
-    priceMultiplicator: Number,
-    actualPrice: Number,
-    obtentionLevel: Number,
-    EPS: Number
+    nameProp: String,
+    earningProp: Number,
+    buildsCountProp: Number,
+    basePriceProp: Number,
+    obtentionLevelProp: Number
+  },
+  data() {
+    return  {
+      name: this.nameProp,
+      earning: this.earningProp,
+      buildsCount: this.buildsCountProp,
+      basePrice: this.basePriceProp,
+      obtentionLevel: this.obtentionLevelProp,
+      pricePercentUpper: 15,
+    }
+  },
+  computed: {
+    actualPrice() {
+      return Math.round((this.basePrice + this.basePrice * this.pricePercentUpper/100) * this.buildsCount ) || this.basePrice;
+    },
+    EPS() {
+      return this.earning * this.buildsCount;
+    },
   },
   methods: {
     buy(){
       console.log("buy");
+      this.buildsCount++;
     },
     setEPS(){
       console.log("setting the global EPS");
     }
+  },
+  mounted() {
+    console.log(this.basePrice,  this.buildsCount, this.priceMultiplicator,  this.basePrice * this.buildsCount * this.priceMultiplicator)
   }
 }
 </script>
--- clicker/src/views/Tab3.vue
+++ clicker/src/views/Tab3.vue
@@ -2,7 +2,15 @@
   <ion-page>
     <ion-content :fullscreen="true">
       <div class="tab-content">
-
+        <item 
+          v-for="(item, index) in items" :key="item.name"
+          :nameProp="item.name"
+          :earningProp="item.earning"
+          :buildsCountProp="buildsCountData[index]"
+          :basePriceProp="item.basePrice"
+          :obtentionLevelProp="item.obtentionLevel"
+        >
+        </item>
       </div>
     </ion-content>
   </ion-page>
@@ -10,9 +18,27 @@
 
 <script lang="ts">
 import { IonPage, IonContent } from '@ionic/vue';
+import Item from '../components/Item.vue';
 
 export default  {
   name: 'Tab3',
-  components: { IonContent, IonPage }
+  data: () => ({
+    items: [
+      {
+        name: "Tipi",
+        earning: 1,
+        obtentionLevel: 1,
+        basePrice: 100,
+      },
+      {
+        name: "Cabane",
+        earning: 2,
+        obtentionLevel: 2,
+        basePrice: 1100,
+      }
+    ],
+    buildsCountData: [0, 0]
+  }),
+  components: { IonContent, IonPage, Item }
 }
 </script>
